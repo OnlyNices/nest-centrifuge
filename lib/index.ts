74da2acfd2a32c81f069c1c51e6dd9d85b33f0a0
Sledgeee <bazisc.qa@gmail.com>
2024-10-06 16:34:04 +0300

    Update types exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
 export * from "./centrifuge.module";
 export * from "./centrifuge.service";
 export { CENTRIFUGE_CLIENT } from "./centrifuge.constants";
-export { CentrifugeClient } from "./centrifuge.type";
+export * from "./centrifuge.type";
